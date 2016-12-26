8cde661fb7bac8a7bab97f6bfccb8d25992b3058
icexcris <xcris22@gmail.com>
2016-12-27 00:20:21 +0200

    template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,139 +3,255 @@
 @section('content') 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-9">
             <h3>
-                News
+                News 
             </h3>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h3>
+               Games
+            </h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <h3>
                 Funny
             </h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail First" src="http://lorempixel.com/output/people-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Second" src="http://lorempixel.com/output/city-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="thumbnail">
+                        <img alt="Bootstrap Thumbnail Third" src="http://lorempixel.com/output/sports-q-c-600-200-1.jpg" />
+                        <div class="caption">
+                            <h3>
+                                Thumbnail label
+                            </h3>
+                            <p>
+                                Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                            </p>
+                            <p>
+                                <a class="btn btn-primary" href="#">Read more</a> <a class="btn" href="#">Read more</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-3">
+        <div class="row">
             <h3>
-                Games
+                Recent Posts
             </h3>
+            <ol>
+                <li>
+                    Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                    Consectetur adipiscing elit
+                </li>
+                <li>
+                    Integer molestie lorem at massa
+                </li>
+                <li>
+                    Facilisis in pretium nisl aliquet
+                </li>
+                <li>
+                    Nulla volutpat aliquam velit
+                </li>
+                <li>
+                    Faucibus porta lacus fringilla vel
+                </li>
+                <li>
+                    Aenean sit amet erat nunc
+                </li>
+                <li>
+                    Eget porttitor lorem
+                </li>
+            </ol>
+            <h3>
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+            <ol>
+                <li>
+                    Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                    Consectetur adipiscing elit
+                </li>
+                <li>
+                    Integer molestie lorem at massa
+                </li>
+                <li>
+                    Facilisis in pretium nisl aliquet
+                </li>
+                <li>
+                    Nulla volutpat aliquam velit
+                </li>
+                <li>
+                    Faucibus porta lacus fringilla vel
+                </li>
+                <li>
+                    Aenean sit amet erat nunc
+                </li>
+                <li>
+                    Eget porttitor lorem
+                </li>
+            </ol>
+            <ul>
+                <li>
+                    Lorem ipsum dolor sit amet
+                </li>
+                <li>
+                    Consectetur adipiscing elit
+                </li>
+                <li>
+                    Integer molestie lorem at massa
+                </li>
+                <li>
+                    Facilisis in pretium nisl aliquet
+                </li>
+                <li>
+                    Nulla volutpat aliquam velit
+                </li>
+                <li>
+                    Faucibus porta lacus fringilla vel
+                </li>
+                <li>
+                    Aenean sit amet erat nunc
+                </li>
+                <li>
+                    Eget porttitor lorem
+                </li>
+            </ul>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <img alt="Bootstrap Image Preview" src="http://lorempixel.com/140/140/" />
-            <h2>
-                Heading
-            </h2>
-            <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-            </p>
-            <p>
-                <a class="btn" href="#">View details »</a>
-            </p>
-        </div>
     </div>
 </div>
 @endsection
