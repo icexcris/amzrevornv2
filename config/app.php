6b57d6d3a43b4859602a27abf5c80eb73cf9f3d7
icexcris <xcris22@gmail.com>
2016-12-26 14:25:05 +0200

    Cokie consent
    cokie consent

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         Ipunkt\LaravelAnalytics\AnalyticsServiceProvider::class,
+        Spatie\CookieConsent\CookieConsentServiceProvider::class,
 
     ],
 
