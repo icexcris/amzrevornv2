18a5fe330e66f59bcc1a75abf31a31d292f2b1da
icexcris <xcris22@gmail.com>
2016-12-27 00:22:09 +0200

    template

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,36 +1,6 @@
 <footer>
 
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6">
-		Social 
-		</div>
-		<div class="col-md-6">
-			<div class="btn-group btn-group-md btn-group-orizontal">
 
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-left"></em> Left
-				</button> 
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-left"></em> Left
-				</button> 
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-center"></em> Center
-				</button>
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-center"></em> Center
-				</button> 
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-right"></em> Right
-				</button> 
-				<button class="btn btn-default" type="button">
-					<em class="glyphicon glyphicon-align-right"></em> Right
-				</button> 
-				
-			</div>
-		</div>
-	</div>
-</div>
 	<div class="container-fluid">
 	
 		
@@ -43,6 +13,6 @@
 	</div>
 </div>
 
- <div class="container"> <div class="row"> <div class="col-sm-6"><ul class="nav nav-tabs"><li class=""><a href="#">Home</a></li><li class=""><a href="#">Profile</a></li><li class="active"><a href="#">Messages</a></li><li><a href="#">Nav item</a></li><li><a href="#">Nav item</a></li><li><a href="#">Nav item</a></li></ul></div><div class="col-sm-6"> <p class="text-info text-right"> <br><br></p><div class="row"> <div class="col-md-12 hidden-lg hidden-md hidden-sm text-left"> <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-github text-inverse"></i></a> </div></div><div class="row"> <div class="col-md-12 hidden-xs text-right"> <a href="#"><i class="fa fa-3x fa-fw fa-instagram text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-twitter text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-facebook text-inverse"></i></a> <a href="#"><i class="fa fa-3x fa-fw fa-github text-inverse"></i></a> </div></div></div></div></div>
+
 </footer>
 
