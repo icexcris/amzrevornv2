61f10a16675e58fb2cf2d46d88d49b6f3af1a65c
icexcris <xcris22@gmail.com>
2016-12-27 11:26:14 +0200

    template

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,44 @@
 <footer>
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6">
 
-	<div class="container-fluid">
-	
-		
+		<div class="col-md-12">
+		Follow us: 
+</div>
+		 <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-facebook"><i class="fa fa-facebook"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-flickr"><i class="fa fa-flickr"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-google-plus"><i class="fa fa-google-plus"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-instagram"><i class="fa fa-instagram"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-pinterest"><i class="fa fa-pinterest"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-tumblr"><i class="fa fa-tumblr"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-twitter"><i class="fa fa-twitter"></i></a>
+        <a href="http://scripteden.com/download/eden-ui/" target="_blank" class="btn-social btn-youtube"><i class="fa fa-youtube"></i></a>
+
+                    </div>
+
+
+
+
+
+
+
+		<div class="col-md-6">
+		<div class="btn-group">
+    <button type="button" class="btn btn-site">Contact</button>
+    <button type="button" class="btn btn-site">About us</button>
+    <button type="button" class="btn btn-site">Privacy Policy</button>
+    <button type="button" class="btn btn-site">Terms and Conditions</button>
+    <button type="button" class="btn btn-site">Rss</button>
+    <button type="button" class="btn btn-site">Sitempas</button>
+  </div>
+		</div>
+	 </div>
+</div>
+
+
+	<div class="container-fluid">		
 	<div class="row">
 		<div class="col-md-12">
 			<h3 class="text-center">
