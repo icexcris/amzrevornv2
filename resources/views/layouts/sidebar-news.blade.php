68f3d11d5002395f7ce0e199de2fe9b5df4afce5
icexcris <xcris22@gmail.com>
2016-12-30 23:53:53 +0200

    modal

--- resources/views/layouts/sidebar-news.blade.php
+++ resources/views/layouts/sidebar-news.blade.php
@@ -4,7 +4,10 @@
                 <!-- Blog Search Well -->
                 <div class="well">
                 <h4 class="text-center">Don't want to miss updates? Please click the below button!</h4>
-        <button class="btn btn-site btn-lg center-block">Subscribe to my feed</button>
+        
+        <button class="btn btn-site btn-lg center-block"  title="Share this content" data-toggle="modal" data-target="#myModal" center-block >
+  <i class="fa fa-envelope-o"></i> Newsletter
+</button>
          </div>
         <div>
     <div class="well">
@@ -69,5 +72,43 @@
            
             </div>
       </aside>
+
+
+</div>
+<div class="container">
+    <div class="row">
+        <!-- Button trigger modal -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+        <h4 class="modal-title" id="myModalLabel"><i class="fa fa-envelope"></i>Newsletter</h4>
+      </div>
+      <div class="modal-body">
+
+        
+                       
+                <p>Subscribe to our weekly Newsletter and stay tuned.</p>
+                
+                <form action="" method="post">
+                    <div class="input-group">
+  <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
+  <input type="email" class="form-control" placeholder="your@email.com">
+</div>
+                    <br />
+                    <button type="submit" value="sub" name="sub" class="btn btn-site"><i class="fa fa-share"></i> Subscribe Now!</button>
+              </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-site" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
 </div>
    
