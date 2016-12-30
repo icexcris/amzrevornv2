bdcdb87efc1e2c6c8b7d9e3d1feedbe9a680651a
icexcris <xcris22@gmail.com>
2016-12-31 01:29:11 +0200

    ultim

--- resources/views/layouts/top_nav.blade.php
+++ resources/views/layouts/top_nav.blade.php
@@ -50,9 +50,17 @@
         
       </ul>
       <ul class="nav navbar-nav navbar-right">
-
-        <li><a href="{{ url('/login') }}">Login</a></li>
-        <li><a href="{{ url('/register') }}">Register</a></li>
+        @if(Auth::check())
+<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                  </ul>
+                </li>        @else
+          <li><a href="{{ url('/login') }}">Login</a></li>
+          <li><a href="{{ url('/register') }}">Register</a></li>
+        @endif
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
