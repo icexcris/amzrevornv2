011e94a39b1608648ed4b91886209159348b29f1
icexcris <xcris22@gmail.com>
2017-01-09 00:22:25 +0200

    new user model

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use App\Http\Requests\UserRequest;
+use Auth;
+use App\User;
 class UserController extends Controller
 {
 public function edit(){
 
     	return view('/page/editprofile');
+	}
+
+public function editsave(){
+		
+
+    	return redirect()->route('user.editprofile');
 	}
 
 public function profile(){
