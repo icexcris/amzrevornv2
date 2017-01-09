8751c9f6c714b4f6f18e16e38281188bb833d688
icexcris <xcris22@gmail.com>
2017-01-09 11:26:13 +0200

    pROFILE

--- resources/views/page/editprofile.blade.php
+++ resources/views/page/editprofile.blade.php
@@ -1,103 +1,75 @@
 @extends('layouts.master')
 @section('content') 
 <div class="row">
-      <div class="col-md-12">
+<div class="col-md-8 col-md-offset-2">
             
 
 <div class="panel panel-white post panel-shadow">
 <div class="container-fluid">
-    <h1 class="text-center">Edit Profile</h1>
+    <h1 class="text-center">Account Setings</h1>
   	<hr>
 	<div class="row">
-      <!-- left column -->
-      <div class="col-md-3">
-      <div class="row">
-<center>
-      <h3>Avatar</h3>
       
-       <div class="useravatar" class="text-center">
-            <img id="avatar" alt=""  width="100" height="100" class="img-circle" src="{{ Auth::user()->avatar }}">
-            <input class="hide" id="avatarchose" type="file">
-            </div>
-        </center>
-      </div>
-      </div>
+          <!-- Nav tabs -->
+  <ul class="nav nav-pills nav-justified" role="tablist">
+    <li role="presentation" class="active"><a href="#account" aria-controls="account" role="tab" data-toggle="tab">Acoount</a></li>
+    <li role="presentation"><a href="#password" aria-controls="password" role="tab" data-toggle="tab">Password</a></li>
+    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+    
+  </ul>
+  <hr>
 
-
-      
-         
-
-  
-      
-      <!-- edit form column -->
-      <div class="col-md-9 personal-info">
-        <div class="alert alert-info alert-dismissable">
-          <a class="panel-close close" data-dismiss="alert">×</a> 
-          <i class="fa fa-coffee"></i>
-          This is an <strong>.alert</strong>. Use this to show important messages to the user.
-        </div>
-        <h3>Personal info</h3>
-        
-        <form class="form-horizontal" role="form">
-         
-         
-         
-         
-          <div class="form-group">
-            
-            <div class="col-md-6">
+  <!-- Tab panes -->
+<div class="tab-content">
+  <div role="tabpanel" class="tab-pane fade in active" id="account">
+ 
+<form class="form-horizontal" role="form">
+        <div class="form-group">         
+            <div class="col-md-8 col-md-offset-2">
               <input class="form-control" type="text" placeholder="Username">
             </div>
           </div>
            <div class="form-group">
-            <div class="col-md-6">
+            <div class="col-md-8 col-md-offset-2">
               <input class="form-control" type="text" placeholder="Email">
             </div>
           </div>
-          <div class="form-group">
-           
+          
+           <div class="form-group">
+            <label class="col-md-3 control-label"></label>
             <div class="col-md-6">
+              <input type="button" class="btn btn-site" value="Save Changes">
+              <span></span>
+              <input type="reset" class="btn btn-default" value="Cancel">
+            </div>
+          </div>
+          
+          </form>
+          </div>
+
+
+
+  <div role="tabpanel" class="tab-pane fade" id="password">
+  <form class="form-horizontal" role="form">
+  <div class="form-group">
+           
+            <div class="col-md-8 col-md-offset-2">
               <input class="form-control" type="password" placeholder="Old Password">
             </div>
           </div>
           <div class="form-group">
            
-            <div class="col-md-6">
+            <div class="col-md-8 col-md-offset-2">
               <input class="form-control" type="password" placeholder="New Password">
             </div>
           </div>
           <div class="form-group">
             
-            <div class="col-md-6">
+            <div class="col-md-8 col-md-offset-2">
               <input class="form-control" type="password" placeholder="Re-type New Password">
             </div>
           </div>
-          <div class="form-group">
-          <div class="col-md-6">
-                    
-                
-           <input type="date" id="birthDate" class="form-control">
-           </div>
-           </div>
-          <div class="form-group">
-            
-            <div class="col-md-6">
-              <div class="btn-group" data-toggle="buttons">
-                <label class="btn btn-default">
-                    <input type="radio" name="gender" value="male" /><i class="fa fa-male"></i>
-                </label>
-                <label class="btn btn-default">
-                    <input type="radio" name="gender" value="female" /><i class="fa fa-female"></i>
-                </label>
-               
-            </div>
-          </div>
-          </div>
-          
-           
-
-
-          <div class="form-group">
+           <div class="form-group">
             <label class="col-md-3 control-label"></label>
             <div class="col-md-8">
               <input type="button" class="btn btn-site" value="Save Changes">
@@ -105,14 +77,89 @@
               <input type="reset" class="btn btn-default" value="Cancel">
             </div>
           </div>
-        </form>
+          </form>
+
+    </div>
+  <div role="tabpanel" class="tab-pane fade" id="profile">
+  
+  <form class="form-horizontal" role="form">
+  
+  <div class="form-group">
+  <div class="col-md-8 col-md-offset-2">
+
+      
+      
+  <div class="useravatar" class="text-center">
+            <img id="avatar" alt=""  width="100" height="100"  src="{{ Auth::user()->avatar }}">
+            <input class="hide" id="avatarchose" type="file">
+            </div>
+     
+</div>
+</div>
+         <div class="form-group">
+          <div class="col-md-8 col-md-offset-2">
+                    
+                
+           <input type="date" id="birthDate" class="form-control">
+           </div>
+           </div>
+
+           <div class="form-group">
+<div class="col-md-8 col-md-offset-2">
+         
+          <select name="country" id="country-input" class="form-control" placeholder="Country">
+            
+              <option selected >Country</option>
+             
+              <option></option>
+             
+          </select>
+           
+        </div>
+ </div>
+
+ <div class="form-group">
+ <div class="col-md-8 col-md-offset-2">
+         
+          <textarea name="description" class="form-control" id="yourDescriptionInput" placeholder="Tell people who you are"></textarea>
+        </div>
+        </div>
+          <div class="form-group">
+            
+            <div class="col-md-8 col-md-offset-2">
+              <div class="btn-group" data-toggle="buttons">
+                <label class="btn btn-default">
+                    <input type="radio" name="gender" value="male" /><i class="fa fa-male"></i>
+                </label>
+                <label class="btn btn-default">
+                    <input type="radio" name="gender" value="female" /><i class="fa fa-female"></i>
+                </label>
+</div>
+</div>
+</div>
+
+                 <div class="form-group">
+            <label class="col-md-3 control-label"></label>
+            <div class="col-md-8">
+              <input type="button" class="btn btn-site" value="Save Changes">
+              <span></span>
+              <input type="reset" class="btn btn-default" value="Cancel">
+            </div>
+          </div>
+          </form>
+          </div>
+          </div>
+
+        
       </div>
-  </div>
+        </div>
+      
+       </div>
+     
+      </div>
+  
 </div>
-<hr>
-</div>
-</div>
-</div>
+
 
 
 @endsection
